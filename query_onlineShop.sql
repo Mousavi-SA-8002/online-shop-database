5b4177b78a91b76c2c51e7b75bdf46fbf9074cf6
Seyed Ali Mousavi <76827212+mmdreza00mirrashid@users.noreply.github.com>
2023-01-08 16:19:19 +0330

    adding more queries
    Co-Authored-By: Seyed Ali Mousavi <121666004+Mousavi-SA-8002@users.noreply.github.com>

--- query_onlineShop.sql
+++ query_onlineShop.sql
@@ -29,5 +29,51 @@ group by customer_id;
 
 /*نمایش لیست پیشنهادات ویژه*/
 SELECT product.Name
- FROM `online shop`.shop_item ,`online shop`.product ,`online shop`.product_model
+FROM `online shop`.shop_item ,`online shop`.product ,`online shop`.product_model
  where product_id=product_model_id=product_model_product_id And shop_item.discount>15;
+
+
+
+/*نمایش لیست فروشنده های یک آیتم */
+SELECT product.Name ,shop.name
+FROM `online shop`.shop_item ,`online shop`.shop,`online shop`.product ,`online shop`.product_model
+where product_id=product_model_id=product_model_product_id and shop_item.shop_id=shop_id
+group by product_id;
+
+
+
+/* نمایش 10 سفارش آخر کاربر  */
+select *
+from `online shop`.orders
+where orders.id in(
+    select order_id
+    from orders,wallet_transactions,wallet,customer
+    where wallet_transactions.order_id=order_id and wallet.id=wallet_transactions.wallet_id AND wallet.customer_id=customer_id
+    order by orders.datetime desc
+    limit 10
+
+    )
+
+
+
+/* کاربران مربوط به یک شهر*/
+SELECT distinct customer.full_name ,address.city
+FROM `online shop`.customer  ,`online shop`.address
+where  customer.id=address.profile_id and address.city='مشهد';
+
+
+
+
+/*تامین کنندگان کالا مربوط به یک شهر*/
+select distinct supplier.name
+from `online shop`.supplier
+where supplier.cty='مشهد';
+
+
+
+
+/*نظرات داده شده در رابطه با محصول*/
+select comment.text
+from `online shop`.shop_item , `online shop`.comment
+where shop_item.id=comment.shop_item.id
+order by comment.date;
