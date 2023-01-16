e933f33b1120d23262fd86c84c60fccc66cf407c
Seyed Ali Mousavi <mousavi.sa8002@gmail.com>
2023-01-16 15:09:13 +0330

    add more insert
    Co-Authored-By: FarokhReza <79958424+FarokhReza@users.noreply.github.com>

--- insert.sql
+++ insert.sql
@@ -1,36 +1,51 @@
-INSERT INTO `online shop`.`category`
-(`name`)
-VALUES
-('کالای دیجیتال');
+/******************************************************/
+--                     category
+/******************************************************/
 
+INSERT INTO `online shop`.`category`(id, name)
+VALUES (1,'کالای دیجیتال');
 
-INSERT INTO `online shop`.`category`
-(`name`,
-`parent_category_id`)
-VALUES
-('گوشی موبایل',
-1);
+INSERT INTO `online shop`.`category` (id, name, parent_category_id)
+VALUES (11,'لپ تاپ',1),
+       (12,'گوشی هوشمند',1);
 
+INSERT INTO `online shop`.`category` (id, name)
+VALUES (2,'لوازم خانگی');
 
-INSERT INTO `online shop`.`category`
-(`name`,
-`parent_category_id`)
-VALUES
-('گوشی هوشمند',
-2);
+INSERT INTO `online shop`.`category` (id, name, parent_category_id)
+VALUES (21,'لوازم آشپزخانه',2),
+       (22,'لوازم اتاق',2);
 
+INSERT INTO `online shop`.`category` (id, name)
+VALUES (3,'پوشاک');
 
-INSERT INTO `online shop`.`product`
-(`Name`,
-`Brand`,
-`Description`,
-`category_id`)
-VALUES
-('آیفون 13',
-'Apple',
-'Apple iPhone 13',
-3);
+/******************************************************/
+--                     product
+/******************************************************/
 
+INSERT INTO `online shop`.`product` (id, Name, Brand, Description, category_id)
+VALUES (1,'آیفون 13','Apple 13','Apple iPhone 13',12),
+       (2,'آیفون 14','Apple 14','Apple iPhone 14',12);
+
+INSERT INTO `online shop`.`product` (id, Name, Brand, Description, category_id)
+VALUES (11,'ASUS360','Asus','asus360 asus',11),
+       (12,'ZBook15','HP','ZBook15 HP',11);
+
+INSERT INTO `online shop`.`product` (id, Name, Brand, Description, category_id)
+VALUES (21,'یخچال','Samsung','یخچال سامسونگ',21),
+       (22,'گاز','سامسونگ','گاز سامسونگ',21);
+
+INSERT INTO `online shop`.`product` (id, Name, Brand, Description, category_id)
+VALUES (31,'نخت خواب','چوبی','نخت خواب چوبی',22),
+       (32,'کمد','MDF','کمد MDF',22);
+
+INSERT INTO `online shop`.`product` (id, Name, Brand, Description, category_id)
+VALUES (41,'پیراهن','مخمل','پیراهن مخمل',3),
+       (42,'شرت','بتمن','شرت بتمن',3);
+
+/******************************************************/
+--                     product_model
+/******************************************************/
 
 INSERT INTO `online shop`.`product_model`
 (`name`,
@@ -41,6 +56,9 @@ VALUES
 173,
 1);
 
+/******************************************************/
+--                     model_attribute
+/******************************************************/
 
 INSERT INTO `online shop`.`model_attribute`
 (`attribute_name`,
@@ -53,7 +71,6 @@ VALUES
 1,
 1);
 
-
 INSERT INTO `online shop`.`model_attribute`
 (`attribute_name`,
 `attribute_value`,
@@ -65,47 +82,76 @@ VALUES
 1,
 1);
 
+/******************************************************/
+--                     profile
+/******************************************************/
 
-INSERT INTO `online shop`.`profile`
-(`username`,
-`password`,
-`email`,
-`mobile`)
-VALUES
-('dummyusername',
-'dummypassword',
-'dummyemail@dummyserver.com',
-'09998887766');
+INSERT INTO `online shop`.`profile` (id, username, password, email, mobile)
+VALUES (1,'Mousavi','mousavi1234','mousavi@gmail.com','09027628608');
 
+INSERT INTO `online shop`.`profile` (id, username, password, email, mobile)
+VALUES (2,'reza saat','reza@2002','asnaasharyreza89@gmail.com','09309086506');
 
-INSERT INTO `online shop`.`customer`
-(`full_name`,
-`profile_id`)
-VALUES
-('مشتری الکی',
-1);
+INSERT INTO `online shop`.`profile` (id, username, password, email, mobile)
+VALUES (3,'Mirrashid','Mirrashid1234','mirrashid@gmail.com','09220858202');
 
+INSERT INTO `online shop`.`profile` (id, username, password, email, mobile)
+VALUES (4,'Farokh','farokh1234','farokh@gmail.com','09936722536');
 
-INSERT INTO `online shop`.`address`
-(`name`,
-`city`,
-`street`,
-`detail`,
-`postal_code`,
-`profile_id`)
-VALUES
-('خانه',
-'مشهد',
-'کوثر',
-'کثر 10، پلاک 22، واحد 5',
-'9856123247',
-1);
+INSERT INTO `online shop`.`profile` (id, username, password, email, mobile)
+VALUES (5,'Nasehiyan','nasehiyan1234','nasehiyan@gmail.com','09027939237');
 
+/******************************************************/
+--                     customer
+/******************************************************/
+
+INSERT INTO `online shop`.`customer` (id, full_name, profile_id)
+VALUES (1,'Seyed Ali Mousavi',1);
+
+INSERT INTO `online shop`.`customer` (id, full_name, profile_id)
+VALUES (2,'Reza Asnaashary',2);
+
+INSERT INTO `online shop`.`customer` (id, full_name, profile_id)
+VALUES (3,'MohammadReza Mirrashid',3);
+
+INSERT INTO `online shop`.`customer` (id, full_name, profile_id)
+VALUES (4,'Reza farokh',4);
+
+INSERT INTO `online shop`.`customer` (id, full_name, profile_id)
+VALUES (5,'Mahdi Nasehiyan',5);
+
+/******************************************************/
+--                     address
+/******************************************************/
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
+VALUES (1,'خونه صیاد','مشهد','صیاد شیرازی','صیاد شیرازی 55','9178916879',1);
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
+VALUES (2,'خونه هفت تیر','مشهد','هفت تیر','هفت تیر 11','9133316879',2);
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
+VALUES (3,'خونه دلاوران','مشهد','دلاوران','دلاوران 28','9133311234',3);
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
+VALUES (4,'خونه آزادی','تهران','آزادی','آزادی 59','8533311234',4);
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
+VALUES (5,'خونه اندیشه','کرمان','اندیشه','اندیشه 87','4533251234',5);
+
+/******************************************************/
+--                     shop
+/******************************************************/
 
 INSERT INTO `online shop`.`shop`
 (`name`)
 VALUES
 ('فروشگاه موبایل الکی');
+
+/******************************************************/
+--                     wallet
+/******************************************************/
+
 INSERT INTO `online shop`.`wallet`
 (`balance`,
 `customer_id`,
@@ -115,6 +161,9 @@ VALUES
 1,
 1);
 
+/******************************************************/
+--                     payment_gateway
+/******************************************************/
 
 INSERT INTO `online shop`.`payment_gateway`
 (`name`,
@@ -129,20 +178,34 @@ VALUES
 'dummygatewaytoken',
 1);
 
+/******************************************************/
+--                     invoice
+/******************************************************/
+
+insert into `online shop`.invoice (amount, status, gateway_authority, reference_id, order_id, payment_gateway_id)
+                            VALUES (7000    ,1      ,'melat'        ,'melat.ir'     ,1         ,1);
+
+INSERT INTO `online shop`.`invoice`(amount, status, gateway_authority, reference_id, order_id, payment_gateway_id)
+VALUES (100000,1,'dummygatewayauthority','dummypaymentreferenceid',1,1);
 
 INSERT INTO `online shop`.`invoice`
 (`amount`,
 `status`,
 `gateway_authority`,
 `reference_id`,
+`order_id`,
 `payment_gateway_id`)
 VALUES
-(100000,
+(51000,
 1,
-'dummygatewayauthority',
-'dummypaymentreferenceid',
+'dummygatewayauthority2',
+'dummypaymentreferenceid2',
+1,
 1);
 
+/******************************************************/
+--                     wallet_transactions
+/******************************************************/
 
 INSERT INTO `online shop`.`wallet_transactions`
 (`amount`,
@@ -157,6 +220,9 @@ CURDATE(),
 1,
 1);
 
+/******************************************************/
+--                     shop_item
+/******************************************************/
 
 INSERT INTO `online shop`.`shop_item`
 (`product_model_id`,
@@ -173,6 +239,9 @@ VALUES
 50000,
 0);
 
+/******************************************************/
+--                     orders
+/******************************************************/
 
 INSERT INTO `online shop`.`orders`
 (`datetime`,
@@ -197,6 +266,9 @@ VALUES
 1,
 1);
 
+/******************************************************/
+--                     order_items
+/******************************************************/
 
 INSERT INTO `online shop`.`order_items`
 (`orders_id`,
@@ -218,17 +290,61 @@ VALUES
 50000);
 
 
-INSERT INTO `online shop`.`invoice`
-(`amount`,
-`status`,
-`gateway_authority`,
-`reference_id`,
-`order_id`,
-`payment_gateway_id`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO `online shop`.`address` (idaddress, name, city, street, detail, postal_code, profile_id)
 VALUES
-(51000,
+(2,'خانه' ,
+'سبزوار',
+'معلم',
+'معلم 10، پلاک 21، واحد 2',
+'6556123247',
+2);
+INSERT INTO `online shop`.`shop`
+(`name`)
+VALUES
+('فروشگاه موبایل واقعی');
+INSERT INTO `online shop`.`wallet`
+(`balance`,
+`customer_id`,
+`customer_profile_id`)
+VALUES
+(500000,
+2,
+2);
+
+INSERT INTO `online shop`.`invoice` (amount, status, gateway_authority, reference_id, order_id, payment_gateway_id)
+VALUES
+(800000,
 1,
-'dummygatewayauthority2',
-'dummypaymentreferenceid2',
-1,
-1);
+'mmdrezaygatewayauthority',
+'mmdrezamentreferenceid',
+1,1);
+INSERT INTO `online shop`.`wallet_transactions`
+(`amount`,`datetime`,
+`balance`,
+`wallet_id`,
+`invoice_id`)
+VALUES
+(700000,
+CURDATE(),
+100000,
+2,
+2);
+
+insert into `online shop`.wallet_transactions
+    (amount, datetime, balance, wallet_id, invoice_id)
+values (500 , curdate(), 5,      2,          3);
