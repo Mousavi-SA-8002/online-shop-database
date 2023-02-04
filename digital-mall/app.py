a186117b274d7e403869f42adfe0e7e1c65f91a6
Mahdi Nasehiyan <im.insightman@gmail.com>
2023-02-05 03:21:41 +0330

    some static endpoints added

--- digital-mall/app.py
+++ digital-mall/app.py
@@ -66,24 +66,78 @@ def register_api(app, model, name):
 
 def dict_result(result):
     if result is not None:
-        return [{key: value for key, value in row.items()} for row in result if row is not None]
+        return [dict(e._mapping) for e in result if e is not None]
     else:
         return [{}]
 
-@app.route('/top_10_users')
-def top_10_users():
-    return jsonify(
-        [dict(e._mapping)
-        for e in
-        db.session.execute(text('''
-    SELECT c.id, c.full_name, SUM(ABS(wt.amount)) as total_expense
-FROM wallet_transactions as wt JOIN wallet as w ON wt.wallet_id=w.id
-JOIN customer as c ON w.customer_id=c.id
-WHERE wt.datetime > (select now() - interval 30 day)
-GROUP BY c.id, c.full_name
-ORDER BY total_expense DESC
+@app.route('/top_10_users/')
+@app.route('/top_10_users/<int:period>')
+def top_10_users(period=30):
+    result = db.session.execute(text('''
+        SELECT c.id, c.full_name, SUM(ABS(wt.amount)) as total_expense
+        FROM wallet_transactions as wt JOIN wallet as w ON wt.wallet_id=w.id
+        JOIN customer as c ON w.customer_id=c.id
+        WHERE wt.datetime > (select now() - interval {} day)
+        GROUP BY c.id, c.full_name
+        ORDER BY total_expense DESC
+    '''.format(period))).all()
+    return jsonify(dict_result(result))
+
+
+@app.route('/special_offers/')
+def special_offers():
+    result = db.session.execute(text('''
+        SELECT s.name as shop_name,
+        p.name as product_name,
+        pm.name as product_model,
+        si.discount as discount
+        FROM shop as s
+        JOIN shop_item as si ON si.shop_id=s.id
+        JOIN product_model as pm ON pm.id=si.product_model_id
+        JOIN product as p ON p.id=pm.product_id
+        WHERE si.discount > 15
     ''')).all()
-        ])
+    return jsonify(dict_result(result))
+
+@app.route('/shops_having_product/<int:product_id>')
+def shops_having_product(product_id):
+    result = db.session.execute(text('''
+        SELECT s.id, s.name
+        FROM shop as s
+        WHERE s.id IN (SELECT distinct(s.id)
+        FROM shop as s
+        JOIN shop_item as si ON s.id=si.shop_id
+        JOIN product as p ON si.product_model_product_id=p.id
+        WHERE p.id={})
+    '''.format(product_id))).all()
+    return jsonify(dict_result(result))
+
+@app.route('/user_orders/<int:user_id>')
+def user_orders(user_id):
+    result = db.session.execute(text('''
+        SELECT * FROM orders
+        WHERE address_profile_id={}
+        ORDER BY datetime DESC
+    '''.format(user_id))).all()
+    return jsonify(dict_result(result))
+
+@app.route('/customers_of_city/<string:city>')
+def customers_of_city(city):
+    result = db.session.execute(text('''
+        SELECT distinct customer.full_name ,address.city
+        FROM customer, address
+        where  customer.id=address.profile_id and address.city='{}';
+    '''.format(city))).all()
+    return jsonify(dict_result(result))
+
+@app.route('/suppliers_of_city/<string:city>')
+def suppliers_of_city(city):
+    result = db.session.execute(text('''
+        select distinct supplier.name
+        from supplier
+        where supplier.city='{}';
+    '''.format(city))).all()
+    return jsonify(dict_result(result))
 
 if __name__ == "__main__":
     from models import *
